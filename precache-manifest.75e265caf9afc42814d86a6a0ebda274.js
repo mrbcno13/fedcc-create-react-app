7c48a1705cdcda168c8c0fa4ddc37dad5bfe94e4
mrbcno13 <ericnicholoacruz@yahoo.com>
2020-06-03 00:41:34 +0800

    Updates

---
+++ precache-manifest.75e265caf9afc42814d86a6a0ebda274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3511704f98af84ebe97915aae88f726",
+    "url": "/fedcc-create-react-app/index.html"
+  },
+  {
+    "revision": "4b8b1760a87b31a316c8",
+    "url": "/fedcc-create-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0436eb018d07df546cc9",
+    "url": "/fedcc-create-react-app/static/js/2.bc2f5d50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fedcc-create-react-app/static/js/2.bc2f5d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8b1760a87b31a316c8",
+    "url": "/fedcc-create-react-app/static/js/main.facce471.chunk.js"
+  },
+  {
+    "revision": "b237917d72d80bc65e11",
+    "url": "/fedcc-create-react-app/static/js/runtime-main.1dcfa2f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fedcc-create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
